be8cedb6dc6f0dc203d5f412f4647fcccbabe9ae
Lee Changseok <leechangseok@Lees-MacBook-Pro.local>
2019-03-07 21:18:35 -0500

    node express and node html build

--- app.js
+++ app.js
@@ -5,8 +5,22 @@ const port = 3030;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\nLegend\nHate Speech\nSex on the beach\n'); // res.and is like a php echo statment
+  res.setHeader('Content-Type', 'text/html');
+
+  // this is the incoming roiute, just like in Vue
+  let url = req.url;
+
+  if(url == "/about"){
+    res.write(`<h1>You're on the ${url} page!</h1>`)
+    res.end();
+  } else if (url === "/contact") {
+
+    res.write(`<h1>You're on the ${url} page!</h1>`)
+    res.end();
+  } else {
+    res.write(`<h1>Hello world! or at least everyone on port ${port} </h1>`)
+    res.end('Hello World\nLegend\nHate Speech\n');
+  }
 });
 
 server.listen(port, hostname, () => {
